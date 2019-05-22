0b064b20d8c8b32b2a7918760a62abb767515e5f
ronwon <aggarwal.mayank49@gmail.com>
2019-05-22 18:30:41 +0530

    Modified file

--- rotateLineAboutMiddle/main.cpp
+++ rotateLineAboutMiddle/main.cpp
@@ -18,6 +18,21 @@ public:
     }
 } p1, p2;
 
+void rotateLine() {
+    int midx = (p1.x+p2.x)/2;
+    int midy = (p1.y+p2.y)/2;
+    float i = 3;
+    while(1) {
+        p1 = rotByI(p1,i,midx,midy);
+        p2 = rotByI(p2,i,midx,midy);
+        cleardevice();
+        line(p1.x,p1.y,p2.x,p2.y);
+        delay(1);
+
+    }
+
+}
+
 int main() {
 
     initwindow(600, 600);
