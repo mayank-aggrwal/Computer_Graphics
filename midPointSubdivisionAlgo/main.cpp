9ada469a70df0f06eca98ea71efc50475aa8eb9c
ronwon <aggarwal.mayank49@gmail.com>
2019-05-24 08:26:32 +0530

    Added clipping polygon

--- midPointSubdivisionAlgo/main.cpp
+++ midPointSubdivisionAlgo/main.cpp
@@ -23,6 +23,19 @@ class point {
     }
 } a, b, c, d, p1, p2;
 
+int sc = 40;
+
+void makeWindow() {
+    a.x = 5*sc;a.y = 0*sc;
+    b.x = 3*sc;b.y = 2*sc;
+    c.x = 5*sc;c.y = 6*sc;
+    d.x = 7*sc;d.y = 4*sc;
+    line(a.x,a.y,b.x,b.y);
+    line(b.x,b.y,c.x,c.y);
+    line(c.x,c.y,d.x,d.y);
+    line(d.x,d.y,a.x,a.y);
+}
+
 int main()
 {
     initwindow(600, 600);
